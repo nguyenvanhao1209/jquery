6471e96d5f3bc3c1970e42c71f96a75587a8e92c
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-12-27 20:18:46 +0700

    Add files via upload

---
+++ bai_1/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	//console.log('done');
+	$('button').click(function(event) {
+		/* Act on the event */
+		$('h1').animate({
+			marginLeft: 500},
+			400);
+	});
+
+});
