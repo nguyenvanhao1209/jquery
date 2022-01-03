c3ebc24dd396c9cd7f6c97947100f011e56c1d21
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2022-01-03 19:42:03 +0700

    Update script.js

--- bai_9/js/script.js
+++ bai_9/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	$('.content a').fancybox();
+	TweenMax.staggerFrom(
+			$('.content a'),//phan tu dc chon
+			1,// thoi gian chuyen dong
+			{duration: 1, rotation:360, y:100, stagger: 0.5},
+			0.4 // thoi gian cach nhau giua moi hieu ung
+		);
 });
